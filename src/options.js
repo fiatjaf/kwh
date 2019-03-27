9b91eb0440a9b442728b58872a045c58dc162b49
fiatjaf <fiatjaf@gmail.com>
2019-03-27 08:53:57 -0300

    fix options page saving glitches.

--- src/options.js
+++ src/options.js
@@ -3,7 +3,7 @@
 import browser from 'webextension-polyfill'
 import React, {useState, useEffect} from 'react' // eslint-disable-line
 import {render} from 'react-dom'
-import {useDebounce} from 'use-debounce'
+import debounce from 'debounce-with-args'
 
 const defaultOptions = {
   endpoint: 'http://localhost:9737/rpc',
@@ -12,54 +12,52 @@ const defaultOptions = {
 }
 
 function App() {
-  let [currentOptions, setOptions] = useState(defaultOptions)
-  let [options] = useDebounce(currentOptions, 1500)
+  let [options, setOptions] = useState(defaultOptions)
+  let [initialValues, setInitialValues] = useState(defaultOptions)
   let [saved, setSaved] = useState(false)
 
   useEffect(() => {
-    browser.storage.local.get(defaultOptions).then(setOptions)
+    browser.storage.local.get(defaultOptions).then(values => {
+      setOptions(values)
+      setInitialValues(values)
+    })
   }, [])
 
-  useEffect(
-    () => {
-      var proceed = false
-      for (let k in defaultOptions) {
-        if (options[k] !== defaultOptions[k]) {
-          proceed = true
-          break
-        }
-      }
-      if (!proceed) return
-
-      browser.storage.local.set(options).then(() => {
+  const saveOptions = debounce(
+    function(newValues) {
+      browser.storage.local.set(newValues).then(() => {
         setSaved(true)
         setTimeout(() => {
           setSaved(false)
         }, 2500)
       })
     },
-    [options]
+    900,
+    () => 'options'
   )
 
   function handleChange(e) {
     let k = e.target.name
     let v = e.target.value.trim()
-    setOptions({...options, [k]: v})
+    let newValues = {...options, [k]: v}
+    setOptions(newValues)
+    saveOptions(newValues)
   }
 
   return (
     <div className="flex flex-column ma2 lh-copy f5 sans-serif black-70">
       {[
-        ['Spark URL', 'endpoint'],
-        ['Spark username', 'username'],
-        ['Spark password', 'password']
-      ].map(([label, attr]) => (
-        <div className="ma1 pa1">
+        ['Spark URL', 'endpoint', 'text'],
+        ['Spark username', 'username', 'text'],
+        ['Spark password', 'password', 'password']
+      ].map(([label, attr, type]) => (
+        <div className="ma1 pa1" key={attr}>
           <label className="flex align-center items-center justify-between">
             <span className="w4 pa1 tr">{label}: </span>
             <input
+              type={options[attr] !== initialValues[attr] ? 'text' : type}
               className="pa1 flex-auto black-70"
-              value={currentOptions[attr]}
+              value={options[attr]}
               name={attr}
               onChange={handleChange}
             />
